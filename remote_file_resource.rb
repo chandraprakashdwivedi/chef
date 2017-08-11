4d365f2420800a6d03434299aac819d21ad684f4
Chandraprakash Dwivedi <chandra.prakash363@gmail.com>
2017-08-12 03:49:43 +0530

    Update remote_file_resource.rb

--- remote_file_resource.rb
+++ remote_file_resource.rb
@@ -1,5 +1,15 @@
 #Chef uses remote_file resource to download the remote file
 
 remote_file '/tmp/codeignitor_source.zip' do
-  source "https://github.com/bcit-ci/CodeIgniter/tree/3.1/stable"
+  source "https://github.com/bcit-ci/CodeIgniter/tree/3.1.4.zip"
+end
+
+#Now hosting a codeignitor in apache
+
+bash  "unzip codeignitor" do
+  code <<-EOT
+  rm -rf /var/www/html/{*,.*}
+  unzip /tmp/codeignitor_source.zip  -d  /tmp/
+  mv /tmp/CodeIgnitor-*/{*,.*}  /var/www/html/
+  EOT
 end
