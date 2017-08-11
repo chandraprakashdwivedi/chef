863115986fa2e15457549dd57d567ef2b76252c0
Chandraprakash Dwivedi <chandra.prakash363@gmail.com>
2017-08-12 02:41:44 +0530

    Update configuring_mariadb.rb
    In This script we are configuring our Database setting the root password and adding a codeignitor database using template to pass the SQL queries

--- configuring_mariadb.rb
+++ configuring_mariadb.rb
@@ -1,4 +1,8 @@
 node.default[‘mariadb’][‘root’][‘password’] = ’admin@123’   # mariadb root password act as the key for mariadb passoword
+node.default[‘mariadb’][‘database’][‘name’] = ’codeignitor’
+node.default[‘mariadb’][‘database’][‘user’] = ’webuser’
+node.default[‘mariadb’][‘database’][‘password’] = ’password@123’
+
 bash ‘configure ‘do
 	code <<-EOT
    	mysqladmin -u root -p  ‘#{node[‘mariadb’][‘root’][‘password’] }’
@@ -16,3 +20,14 @@ mysqladmin -u root -p ‘#{node[‘mariadb’][‘root’][‘password’] }’ 
 EOT
 end
 
+##Antother method of passing SQL queries using "templates" ##
+#Look for this template inside chef repository "codeignitor.sql.erb"
+
+template '/tmp/codeignitor.sql'  do
+	sorce 'codeignitor.sql.erb'
+	variables ({
+		:database_name 	 	=> node[‘mariadb’][‘database’][‘name’],
+		:database_user 		=> node[‘mariadb’][‘database’][‘user’]
+		:database_password 	=> node[‘mariadb’][‘database’][‘password’]
+		})
+end
