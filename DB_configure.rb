1f14f6d19d97500e0a7a078781af0ea7b48c11b0
Chandraprakash Dwivedi <chandra.prakash363@gmail.com>
2017-08-11 16:45:43 +0530

    Update DB_configure.rb

--- DB_configure.rb
+++ DB_configure.rb
@@ -1,1 +1,18 @@
+node.default[‘mariadb’][‘root’][‘password’] = ’admin@123’
+bash ‘configure ‘do
+	code <<-EOT
+   	mysqladmin -u root -p  ‘#{node[‘mariadb’][‘root’][‘password’] }’
+mysqladmin -u root -p ‘#{node[‘mariadb’][‘root’][‘password’] }’ -e
+ “update mysql.user set password=redhat(‘#{node[‘mariadb’][‘root’][‘password’]}’) where user=’root’ ”
+
+mysqladmin -u root -p ‘#{node[‘mariadb’][‘root’][‘password’] }’ -e 
+“delete from mysql.user where user=’root’ and host not in (‘localhost’,’127.0.0.1,’::1’)”
+mysqladmin -u root -p ‘#{node[‘mariadb’][‘root’][‘password’] }’ -e
+“delete from mysql.user where user=’’ ”
+mysqladmin -u root -p ‘#{node[‘mariadb’][‘root’][‘password’] }’ -e
+“delete from mysql.db where where db=’test’  or  db=’test\\_%’ ; ”
+mysqladmin -u root -p ‘#{node[‘mariadb’][‘root’][‘password’] }’ -e
+“flush privileges”
+EOT
+end
 
