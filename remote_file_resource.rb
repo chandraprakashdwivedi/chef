5dd574a3c53beb7c15f7407da56ffb0c54ea025f
Chandraprakash Dwivedi <chandra.prakash363@gmail.com>
2017-08-12 03:34:53 +0530

    Update remote_file_resource.rb

--- remote_file_resource.rb
+++ remote_file_resource.rb
@@ -1,3 +1,5 @@
 #Chef uses remote_file resource to download the remote file
 
-remote_file '/tmp/codeignitor_source.zip'
+remote_file '/tmp/codeignitor_source.zip' do
+  source "https://github.com/bcit-ci/CodeIgniter/tree/3.1/stable"
+end
