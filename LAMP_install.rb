cbee628ed5f4d035bedba956f4562b5ee0cf66fe
Chandraprakash Dwivedi <chandra.prakash363@gmail.com>
2017-08-12 01:55:41 +0530

    Update LAMP_install.rb

--- LAMP_install.rb
+++ LAMP_install.rb
@@ -1,1 +1,30 @@
+%w(httpd mariadb-server ).each  do |p|
+package p do
+	action : install
+end
+end
+
+execute “epel” do
+	not_if  “rpm  -qa  | grep epel”	   #This is just the condition if the package is install it will neglect
+	command “rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm”
+end
+
+execute “webtatic” do
+	not_if  “rpm  -qa  | grep webtatic”
+	command “rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm”
+end
+
+%w(mod_php php-mysql php*).each do |p|
+	package p do
+		action : install
+	end
+end
+
+
+[“httpd”,”mariadb”].each do |p|
+	service p do 
+		action [:start,:enable]
+	end
+end
+
 
