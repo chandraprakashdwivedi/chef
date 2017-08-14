d6aae8284c53dc6e7c7de9c73ecb1dbb9e872aa3
Chandraprakash Dwivedi <chandra.prakash363@gmail.com>
2017-08-14 15:45:48 +0530

    Update environment_1.rb

--- environment_1.rb
+++ environment_1.rb
@@ -1,3 +1,10 @@
 name ‘production’
-description ‘’ This the production environment sciprt”
+description ‘ This the production environment sciprt'
+cookbook 'nginx', '=0.2.0'
+
+
+#similary create a second environment just say testing with different version number
+#name ‘production’
+#description ‘ This the production environment sciprt'
+#cookbook 'nginx', '=0.3.0'
 
