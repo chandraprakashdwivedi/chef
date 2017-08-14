160cc91e22e2aa67538e6d0db2dcde8da55ee6f4
Chandraprakash Dwivedi <chandra.prakash363@gmail.com>
2017-08-14 11:59:42 +0530

    Update custom_recipe.rb

--- custom_recipe.rb
+++ custom_recipe.rb
@@ -1,1 +1,15 @@
+property :rootpath, String, default: '/var/www/localhost'
+action : create do
+  execute "create directory" do
+    command "mkdir -p #{rootpath}"
+    not_if " -d #{rootpath}"
+  end
+  cookbook_file "#{rootpath}/index.html" do
+    source "index.html"
+  end
+end
 
+action : delete do
+  execute "rm -rf #{rootpath}"
+  not_if "[ -d #{rootpath} ]"
+end
