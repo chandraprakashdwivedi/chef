6ad320963701353f3eef1fa3e253d86b943c1c90
Chandraprakash Dwivedi <chandra.prakash363@gmail.com>
2017-08-14 12:13:03 +0530

    Update custom_recipe.rb

--- custom_recipe.rb
+++ custom_recipe.rb
@@ -1,4 +1,8 @@
 property :rootpath, String, default: '/var/www/localhost'
+
+#Here “property” is the variable which we need to define on custom resources “rootpath” is the symbol for the value we pass “string” is the data type of property and default is the value which is to be passed if the user not define any value.
+#Now under this property you can create as many actions as you can.
+
 action : create do
   execute "create directory" do
     command "mkdir -p #{rootpath}"
