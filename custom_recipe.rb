ba047eea6e235228d99a8995ebe935104f10a1ae
Chandraprakash Dwivedi <chandra.prakash363@gmail.com>
2017-08-14 12:41:01 +0530

    Update custom_recipe.rb

--- custom_recipe.rb
+++ custom_recipe.rb
@@ -1,3 +1,6 @@
+resource_name  :resource1
+#To give the name of our resource file
+
 property :rootpath, String, default: '/var/www/localhost'
 
 #Here “property” is the variable which we need to define on custom resources “rootpath” is the symbol for the value we pass “string” is the data type of property and default is the value which is to be passed if the user not define any value.
@@ -17,3 +20,11 @@ action : delete do
   execute "rm -rf #{rootpath}"
   not_if "[ -d #{rootpath} ]"
 end
+
+#After that open your recipe file on which we have to include this custom resource and include these lines
+#resource1 “create the webroot”  do
+#	rootpath “/var/www/mysite”
+#	action :create
+#end
+
+#Note after every custom change make changes on the version of recipe.
